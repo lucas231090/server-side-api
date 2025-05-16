03ad8c7ba62f0dce8f2d583bfb84b8bee5aafa14
Lucas <souza.lucasj@gmail.com>
2025-05-15 23:14:50 -0300

    index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const db = require("./models/ConnectDatabase");
-const routes = require("./routes");
+
 const app = express();
 const port = 3000;
 
+// Conexão com o banco de dados
 db.testConnection().catch((err) => {
   console.error(
     "Não foi possível conectar ao banco de dados. Encerrando o aplicativo."
@@ -13,8 +14,18 @@ db.testConnection().catch((err) => {
 
 app.use(express.json());
 
-app.use(routes);
+// Rotas (antigo routes.js embutido aqui)
+app.get("/", (request, response) => {
+  console.log(request.query);
+  response.json({ message: "Professor Lucas" });
+});
 
+app.post("/users", (request, response) => {
+  console.log(request.body);
+  response.json({ message: "Rota /users METODO POST" });
+});
+
+// Iniciar servidor
 app.listen(port, () => {
   console.log(`Servidor rodando em: http://localhost:${port}`);
 });
